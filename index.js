bf9e93b84c1e76c42da49b703e119281b3f81027
Pierre-Olivier Tharel <pierre.tharel@gmail.com>
2025-01-09 14:12:14 +0100

    Ajout Auth

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ import express from 'express'
 import 'dotenv/config'
 import musicsRouter from './routes/musicsrouter.js'
 import usersRouter from './routes/usersRouter.js'
+import postsRouter from './routes/postsRouter.js'
+import authRouter from './routes/authRouter.js'
 import cors from 'cors'
 import mongoose from 'mongoose'
 
@@ -9,22 +11,23 @@ const app = express()
 const PORT = process.env.PORT || 4000
 
 app.use(cors())
+
 app.use(express.json())
 app.use(express.urlencoded({extended : false}))
-app.use('/api', musicsRouter, usersRouter)
 
+app.use('/api', musicsRouter, usersRouter, postsRouter, authRouter)
 
 app.get('/', (request, response) => {
     response.send(`Welcome to my API`)
 })
 
+
 const mongoDB = process.env.MONGO_URI
-mongoose.connect(mongoDB)
+mongoose.connect(mongoDB, {useNewUrlParser: true});
 
-const db = mongoose.collection
-
+const db = mongoose.connection;
+//Bind connection to error event (to get notification of connection errors)
 db.on("error", console.error.bind(console, "MongoDB connection error:"));
 
-app.listen(PORT, () => console.log(`Server is running on port ${PORT} 🟢`))
 
-// CRUD : Create / Read / Update / Delete
+app.listen(PORT, () => console.log(`Server is running on port ${PORT} 🟢`))
--- models/Users.js
+++ models/Users.js
@@ -1,11 +1,18 @@
 import mongoose, {Schema} from 'mongoose'
 
 
+
 const UserSchema = new Schema({
     first_name : String,
     last_name : String,
-    passion : {
-        type : String,
+    email : {
+        unique : true,
+        required : true,
+        type : String
+    },
+    password : {
+        required : true,
+        type : String
     }
 })
 
